151e59edc0889217820441a1ea52959a60fc3536
keithng <79431961+ngkahinkeith@users.noreply.github.com>
2021-09-27 15:07:21 +0800

    chore: init files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from 'url:./logo.svg';
+import './App.css';
+import { Button } from 'react-toolbox';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Button href='https://www.google.com' target='_blank' raised>
+          Google
+        </Button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
